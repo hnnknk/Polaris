4aba5530ecc02facbfb5b75048aece632a946fbc
hnnknk <nevermoodjo@gmail.com>
2018-04-11 14:23:17 +0700

    Новый проект. Spring boot + data.

---
+++ src/main/java/xyz/hnnknk/polaris/controller/IndexController.java
@@ -0,0 +1,14 @@
+package xyz.hnnknk.polaris.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getIndex() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Polaris</title>
+  </head>
+  <body>
+  Index page
+  </body>
+</html>
